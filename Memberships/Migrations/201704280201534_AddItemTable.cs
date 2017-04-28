150714d78dd27586e4ec4c52c00aa505a9630d21
yliu84 <yliu84@studentmail.nait.ca>
2017-04-27 20:09:58 -0600

    added Entities

---
+++ Memberships/Migrations/201704280201534_AddItemTable.cs
@@ -0,0 +1,16 @@
+namespace Memberships.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddItemTable : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
