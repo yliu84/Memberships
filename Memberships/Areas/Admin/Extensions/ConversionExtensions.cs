25dc42933c01d741051e8e0af39c796e76320115
yliu84 <yliu84@studentmail.nait.ca>
2017-04-28 14:54:00 -0600

    Updated interface of item views

---
+++ Memberships/Areas/Admin/Extensions/ConversionExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Memberships.Areas.Admin.Models;
+using System.Collections;
+using Memberships.Entities;
+using Memberships.Models;
+using System.Data.Entity;
+
+namespace Memberships.Areas.Admin.Extensions
+{
+    public static class ConversionExtensions
+    {
+        public static async Task<IEnumerable<ProductModel>> Convert(
+            this IEnumerable<Product> Products, ApplicationDbContext db) 
+        {
+            var texts = await db.ProductLinkTexts.ToListAsync();
+            var types = await db.ProductTypes.ToListAsync();
+
+            return from p in Products
+                   select new ProductModel
+                   {
+                       Id = p.Id,
+                       Title = p.Title,
+                       Description = p.Description,
+                       ImageUrl = p.ImageUrl,
+                       ProductLinkTextId = p.ProductLinkTextId,
+                       ProductTypeId = p.ProductTypeId,
+                       ProductLinkTexts = texts,
+                       ProductTypes = types
+                   };
+        }
+    }
+}
