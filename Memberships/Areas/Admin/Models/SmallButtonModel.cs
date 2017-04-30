c8e46fb1f932346692ad7322620a2e95a4fd6468
yliu84 <yliu84@studentmail.nait.ca>
2017-04-30 00:40:42 -0600

    Added User controller and view

--- Memberships/Areas/Admin/Models/SmallButtonModel.cs
+++ Memberships/Areas/Admin/Models/SmallButtonModel.cs
@@ -16,6 +16,7 @@ namespace Memberships.Areas.Admin.Models
         public int? ItemId { get; set; }
         public int? ProductId { get; set; }
         public int? SubscriptionId { get; set; }
+        public string UserId { get; set; }
 
         public string ActionParameters { 
             get {
@@ -27,10 +28,13 @@ namespace Memberships.Areas.Admin.Models
                     param.Append(String.Format("{0}={1}&", "ItemId", ItemId));
 
                 if (ProductId != null && ProductId > 0)
-                    param.Append(String.Format("{0}={1}&", "ProductId", ProductId));
+                    param.Append(String.Format("{0}={1}&", "productId", ProductId));
 
                 if (SubscriptionId != null && SubscriptionId > 0)
-                    param.Append(String.Format("{0}={1}&", "SubscriptionId", SubscriptionId));
+                    param.Append(String.Format("{0}={1}&", "subscriptionId", SubscriptionId));
+
+                if (UserId != null && !UserId.Equals(string.Empty))
+                    param.Append(String.Format("{0}={1}&", "userId", UserId));
 
                 return param.ToString().Substring(0,param.Length-1);
             } 
