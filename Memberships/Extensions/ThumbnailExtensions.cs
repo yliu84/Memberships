dbc604fda84bdfd72cca9dd748b5d0cc697abbe2
yliu84 <yliu84@studentmail.nait.ca>
2017-04-30 23:44:10 -0600

    added ThumbnailExtentions

--- Memberships/Extensions/ThumbnailExtensions.cs
+++ Memberships/Extensions/ThumbnailExtensions.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Data.Entity;
 using System.Threading.Tasks;
+using Memberships.Compares;
 
 namespace Memberships.Extensions
 {
@@ -33,6 +34,46 @@ namespace Memberships.Extensions
             { }
 
             return new List<int>();
+        }
+
+        public static async Task<IEnumerable<ThumbnailModel>> GetProductThumbnailsAsync
+        (this List<ThumbnailModel> thumbnails, string userId = null, ApplicationDbContext db=null )
+        {
+            try
+            {
+                if (userId == null)
+                    return new List<ThumbnailModel>();
+
+                if (db == null)
+                    db = ApplicationDbContext.Create();
+
+                var subscriptionIds = await GetSubscriptionIdsAsync(userId, db);
+
+                thumbnails = await (
+                    from ps in db.SubscriptionProducts
+                    join p in db.Products on ps.ProductId equals p.Id
+                    join plt in db.ProductLinkTexts on p.ProductLinkTextId equals plt.Id
+                    join pt in db.ProductTypes on p.ProductTypeId equals pt.Id
+                    where subscriptionIds.Contains(ps.SubscriptionId)
+                    select new ThumbnailModel
+                    {
+                        ProductId = p.Id,
+                        SubscriptionId = ps.SubscriptionId,
+                        Title = p.Title,
+                        Description = p.Description,
+                        ImageUrl = p.ImageUrl,
+                        Link = "/ProductContent/Index/" + p.Id,
+                        TagText = plt.Title,
+                        ContentTag = pt.Title
+                    }).ToListAsync();
+
+            }
+            catch
+            {
+
+            }
+
+            return thumbnails.Distinct(new ThumbnailEqualityComparer()).OrderBy(o => o.Title);
         }
     }
 }
