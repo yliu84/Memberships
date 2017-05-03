0a73b6b45b98dfee4058eee23a9bde6fbadb1cf5
yliu84 <yliu84@studentmail.nait.ca>
2017-05-02 19:05:23 -0600

    Updated SectionExtention view

--- Memberships/Extensions/SectionExtensions.cs
+++ Memberships/Extensions/SectionExtensions.cs
@@ -20,7 +20,7 @@ namespace Memberships.Extensions
                                   join i in db.Items on pi.ItemId equals i.Id
                                   join s in db.Sections on i.SectionId equals s.Id
                                   where p.Id.Equals(productId)
-                                  orderby s.Id
+                                  orderby s.Title
                                   select new ProductSection
                                   {
                                       Id = s.Id,
@@ -35,9 +35,11 @@ namespace Memberships.Extensions
 
             var result = sections.Distinct(new ProductSectionEqualityComparer()).ToList();
 
+            var union = result.Where(r => !r.Title.ToLower().Contains("download")).Union(result.Where(r => r.Title.ToLower().Contains("download")));
+
             var model = new ProductSectionModel
             {
-                Sections = result,
+                Sections = union.ToList(),
                 Title = await (from p in db.Products
                                where p.Id.Equals(productId)
                                select p.Title).FirstOrDefaultAsync()
