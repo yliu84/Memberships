07ea9fecaf6535dabc9e0b12f2dd16bcfd636424
yliu84 <yliu84@studentmail.nait.ca>
2017-05-03 22:04:14 -0600

    added RegisterCode JS

---
+++ Memberships/Controllers/RegisterCodeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Memberships.Extensions;
+
+namespace Memberships.Controllers
+{
+    public class RegisterCodeController : Controller
+    {
+        public async Task<ActionResult> Register(string code)
+        {
+            if(Request.IsAuthenticated)
+            {
+                var userId = HttpContext.GetUserId();
+
+                var registred = await SubscriptionExtensions.RegisterUserSubscriptionCode(userId, code);
+
+                if (!registred)
+                    throw new ApplicationException();
+
+                return PartialView("_RegisterCodePartial");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Memberships/Scripts/RegisterCode.js
@@ -0,0 +1,35 @@
+﻿$(function () {
+    var code = $(".register-code-panel input");
+    function dispayMessage(success, message)
+    {
+        var alert_div = $(".register-code-panel .alert");
+
+        alert_div.text(message);
+
+        if (success)
+            alert_div.removeClass('alert-danger').addClass('alert-success');
+        else
+            alert_div.removeClass('alert-success').addClass('alert-danger');
+
+        alert_div.removeClass('hidden');
+    }
+
+    $(".register-code-panel button").click(function (e) {
+        $(".register-code-panel .alert").addClass('hidden');
+
+        if(code.val().length == 0)
+        {
+            dispayMessage(false, "Enter a code");
+            return;
+        }
+
+        $.post('/RegisterCode/Register',
+            { code: code.val() },
+        function (date) {
+            dispayMessage(true, "The code was successfully added. \n\r Please reload the page.");
+            code.val('');
+        }).fail(function (xlr, status, error) {
+            dispayMessage(false, "Could not register the code");
+        });
+    });
+});
