14aab7105d943dfa9f96049d6970f2f184eeea92
yliu84 <yliu84@studentmail.nait.ca>
2017-05-04 21:04:43 -0600

    Finished Forgot-password part

--- Memberships/Extensions/HtmlExtensions.cs
+++ Memberships/Extensions/HtmlExtensions.cs
@@ -13,15 +13,27 @@ namespace Memberships.Extensions
                                               string action, 
                                               string text, 
                                               string glyphicon, 
-                                              string cssClasses, 
-                                              string id)
+                                              string cssClasses ="", 
+                                              string id ="",
+                                              Dictionary<string,string> attributes = null)
         {
             //declare a span for the glyphicon
             var glyph = string.Format("<span class='glyphicon glyphicon-{0}'></span>",glyphicon);
 
             //declare the anchor tag
             var anchor = new TagBuilder("a");
-            anchor.MergeAttribute("href",string.Format("/{0}/{1}",controller,action));
+
+            if (controller.Length > 0)
+            {
+                anchor.MergeAttribute("href", string.Format("/{0}/{1}", controller, action));
+            }
+            else
+                anchor.MergeAttribute("href", "#");
+
+            if (attributes != null)
+                foreach (var attribute in attributes)
+                    anchor.MergeAttribute(attribute.Key, attribute.Value);
+            
 
             anchor.InnerHtml = string.Format("{0} {1}", glyph, text);
             anchor.AddCssClass(cssClasses);
